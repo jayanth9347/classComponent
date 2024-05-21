65169c4da9f7eb5cdcd28fe56e002649e0da5eff
jayanth <pujalajayanth9347@gmail.com>
2024-05-21 04:18:07 +0000

    main

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Clock from './Newfolder/new'
+
+import {Component} from 'react'
+
+class Job extends Component {
+  render() {
+    return (
+      <div>
+        Hello Im jayanth here....
+        <Clock />
+      </div>
+    )
+  }
+}
+
+export default Job
---
+++ src/Newfolder/new.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+class Clock extends Component {
+  render() {
+    return (
+      <div>
+        <h1> MR.Jayanth </h1>
+      </div>
+    )
+  }
+}
+
+export default Clock
